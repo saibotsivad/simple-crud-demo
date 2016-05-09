81c2b9cd3b69a61e3f633deea8cfa679e0d651e5
Tobias Davis <accounts@davistobias.com>
2016-05-08 20:48:18 -0500

    Delete unused files

--- rollback.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source .config.sh
-
-cp -v .rollback.conf $NGINX_SITES_ENABLED_FOLDER/$SITE.conf
-
-nginx -t
-nginx -s reload
--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source .config.sh
-
-psy stop $SITE:$SPIN
-
-psy start -n $SITE:$SPIN -- node index.js --site=$SITE --spin=$SPIN
