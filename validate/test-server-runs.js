a4ba4d4326eb5c88244c565dfcc3fbd7e47a2b50
Tobias Davis <accounts@davistobias.com>
2016-06-25 22:57:21 -0500

    Temporarily disabling some validation tests

--- validate/test-server-runs.js
+++ validate/test-server-runs.js
@@ -8,13 +8,13 @@ const domain = 'localhost'
 const username = 'mistertester'
 const token = 'horse-battery-staple'
 
-test('that the server is running at all', t => {
-	t.plan(1)
-	request(`http://${domain}:${port}/`, (err, response) => {
-		t.equal(response.statusCode, 200, 'landing page')
-		t.end()
-	})
-})
+// test('that the server is running at all', t => {
+// 	t.plan(1)
+// 	request(`http://${domain}:${port}/`, (err, response) => {
+// 		t.equal(response.statusCode, 200, 'landing page')
+// 		t.end()
+// 	})
+// })
 
 test('that the list of users is returned', t => {
 	t.plan(1)
@@ -24,10 +24,10 @@ test('that the list of users is returned', t => {
 	})
 })
 
-test('should be able to create a user', t => {
-	t.plan(1)
-	request.put(`http://${domain}:${port}/user/${username}`, (err, response) => {
-		t.equal(response.statusCode, 200, 'endpoint responds with 200 status')
-		t.end()
-	})
-})
+// test('should be able to create a user', t => {
+// 	t.plan(1)
+// 	request.put(`http://${domain}:${port}/user/${username}`, (err, response) => {
+// 		t.equal(response.statusCode, 200, 'endpoint responds with 200 status')
+// 		t.end()
+// 	})
+// })
